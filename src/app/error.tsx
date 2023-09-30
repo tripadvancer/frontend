f193eda025f25bb3ab1000e0423ccda857739341
Maksim Oskolskiy <m_oskolskiy@wargaming.net>
2023-09-30 20:26:04 +0300

    Fix error pages

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -6,7 +6,7 @@ import Link from 'next/link'
 export default function Error({ error, reset }: { error: Error & { digest?: string }; reset: () => void }) {
     return (
         <div className="container flex min-h-screen py-16 phone:flex-col phone:px-4 tablet:flex-row tablet:items-center tablet:justify-center tablet:gap-16 desktop:flex-row desktop:items-center desktop:justify-center desktop:gap-32">
-            <Image src="/images/500.svg" width={352} height={406} className="grow phone:hidden" alt="404" />
+            <Image src="/images/500.svg" width={352} height={406} className="grow phone:hidden" alt="404" priority />
             <div className="phone:flex phone:flex-col phone:items-center phone:text-center">
                 <Link href="/">
                     <Image src="/images/logo.svg" width={140} height={24} className="mb-16" alt="Tripadvancer" />
@@ -17,6 +17,7 @@ export default function Error({ error, reset }: { error: Error & { digest?: stri
                     height={406}
                     className="mb-16 phone:w-72 tablet:hidden desktop:hidden"
                     alt="404"
+                    priority
                 />
                 <h2 className="mb-8 text-3xl font-medium">Oops! Something Went Wrong</h2>
                 <p className="mb-8 text-base text-custom-black-70">
--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -10,7 +10,7 @@ export const metadata: Metadata = {
 export default function NotFound() {
     return (
         <div className="container flex min-h-screen py-16 phone:flex-col phone:px-4 tablet:flex-row tablet:items-center tablet:justify-center tablet:gap-16 desktop:flex-row desktop:items-center desktop:justify-center desktop:gap-32">
-            <Image src="/images/404.svg" width={352} height={406} className="grow phone:hidden" alt="404" />
+            <Image src="/images/404.svg" width={352} height={406} className="grow phone:hidden" alt="404" priority />
             <div className="phone:flex phone:flex-col phone:items-center phone:text-center">
                 <Link href="/">
                     <Image src="/images/logo.svg" width={140} height={24} className="mb-16" alt="Tripadvancer" />
@@ -21,6 +21,7 @@ export default function NotFound() {
                     height={406}
                     className="mb-16 phone:w-72 tablet:hidden desktop:hidden"
                     alt="404"
+                    priority
                 />
                 <h2 className="mb-8 text-3xl font-medium">Page not found</h2>
                 <p className="mb-8 text-base text-custom-black-70">
