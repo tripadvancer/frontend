1c48b4744e10f9e9c5dea11a3c58ddc3862b8827
Maksim Oskolskiy <m_oskolskiy@wargaming.net>
2023-10-10 11:30:56 +0300

    .

---
+++ src/services/place.ts
@@ -0,0 +1,29 @@
+import { notFound } from 'next/navigation'
+
+import { IPlace } from '@/types/place'
+
+export async function getPlacesByCountryCode(countryCode: string): Promise<IPlace[]> {
+    const url = process.env.NEXT_PUBLIC_API_URL + '/countries/' + countryCode + '/places'
+    const res = await fetch(url)
+
+    if (!res.ok) {
+        throw new Error('Failed to fetch data')
+    }
+
+    return res.json()
+}
+
+export async function getPlaceById(placeId: string): Promise<IPlace> {
+    const url = process.env.NEXT_PUBLIC_API_URL + '/places/' + placeId
+    const res = await fetch(url)
+
+    if (!res.ok) {
+        if (res.status === 404) {
+            notFound()
+        }
+
+        throw new Error('Failed to fetch data')
+    }
+
+    return res.json()
+}
