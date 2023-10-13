2fe733cc6cd693a35c6adfca1794a5bf0ea36840
Maksim Oskolskiy <m_oskolskiy@wargaming.net>
2023-10-14 00:29:30 +0300

    Global loading state

--- src/app/[locale]/(portal)/layout.tsx
+++ src/app/[locale]/(portal)/layout.tsx
@@ -1,11 +1,11 @@
-import { Header } from '@/components/Header/Header'
 import { Footer } from '@/components/Footer'
+import { Header } from '@/components/Header/Header'
 
 export default function PortalLayout({ children }: { children: React.ReactNode }) {
     return (
         <div className="flex min-h-screen flex-col">
             <Header />
-            <main className="flex-1 px-8 phone:px-4 bg-white -my-8 rounded-t-4xl">{children}</main>
+            <main className="-my-8 flex-1 rounded-t-4xl bg-white px-8 phone:px-4">{children}</main>
             <Footer />
         </div>
     )
--- src/app/[locale]/loading.tsx
+++ src/app/[locale]/loading.tsx
@@ -1,3 +1,10 @@
+import { Spinner } from '@/components/Spinner'
+
 export default function Loading() {
-    return <div>Loading ...</div>
+    return (
+        <div className="flex h-screen w-screen items-center justify-center text-white">
+            <div className="absolute bottom-0 left-0 right-0 top-0 bg-custom-blue-100 opacity-30" />
+            <Spinner size={96} />
+        </div>
+    )
 }
