fa46648892a6e125244c52e939bf4a5ad7e5f7bc
Maksim Oskolskiy <m_oskolskiy@wargaming.net>
2023-10-14 19:14:04 +0300

    Review fetch fix

--- src/services/reviews.ts
+++ src/services/reviews.ts
@@ -5,9 +5,7 @@ import type { IReview } from '@/types/review'
 
 export async function getReviewsByPlaceId(placeId: string): Promise<PaginatedResponse<IReview>> {
     const url = process.env.NEXT_PUBLIC_API_URL + '/reviews?place_id=' + placeId
-    const res = await fetch(url, {
-        next: { tags: ['Reviews'] },
-    })
+    const res = await fetch(url)
 
     if (!res.ok) {
         throw new Error('Failed to fetch data')
@@ -18,7 +16,7 @@ export async function getReviewsByPlaceId(placeId: string): Promise<PaginatedRes
 
 export async function getReviewsByUserId(userId: string, page: string): Promise<PaginatedResponse<IReview>> {
     const url = process.env.NEXT_PUBLIC_API_URL + '/reviews?user_id=' + userId + '&page=' + page
-    const res = await fetch(url, { cache: 'no-store' })
+    const res = await fetch(url)
 
     if (!res.ok) {
         throw new Error('Failed to fetch data')
