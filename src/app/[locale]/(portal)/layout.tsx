f384ebe0270e74c5e18e4946f6612e843c8863e9
Maxim Oskolsky <oskolsky.maxim@gmail.com>
2024-01-25 17:59:07 +0200

    New email verification notice

--- src/app/[locale]/(portal)/layout.tsx
+++ src/app/[locale]/(portal)/layout.tsx
@@ -1,16 +1,15 @@
 import { ReactNode } from 'react'
 
 import { CookieConsent } from '@/components/features/cookie-consent/cookie-consent'
-import { LayoutFooter } from '@/components/features/layout-footer/layout-footer'
-import { LayoutHeader } from '@/components/features/layout-header/layout-header'
+import { Footer } from '@/components/features/footer/footer'
+import { Header } from '@/components/features/header/header'
 
 export default function PortalLayout({ children }: { children: ReactNode }) {
     return (
         <div className="relative flex min-h-screen flex-col">
-            <div className="absolute left-0 right-0 top-0 -z-10 h-[108px] bg-blue-20" />
-            <LayoutHeader />
+            <Header />
             <main className="flex-1 overflow-hidden rounded-t-4xl bg-white">{children}</main>
-            <LayoutFooter />
+            <Footer />
             <CookieConsent />
         </div>
     )
