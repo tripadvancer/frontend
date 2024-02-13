d1e81a214611323ac86f776b33f98c701bd14ecd
Maxim Oskolsky <oskolsky.maxim@gmail.com>
2024-02-13 12:58:58 +0200

    Add link in to widget

--- src/components/features/maps/components/widget/components/widget-place-preview.tsx
+++ src/components/features/maps/components/widget/components/widget-place-preview.tsx
@@ -1,5 +1,7 @@
 'use client'
 
+import Link from 'next/link'
+
 import type { IPlacePreview } from '@/utils/types/place'
 
 import { PlacePreviewActions } from '@/components/ui/place-preview-actions'
@@ -8,10 +10,18 @@ import { PlacePreviewRating } from '@/components/ui/place-preview-rating'
 
 export const WidgetPlacePreview = (place: IPlacePreview) => {
     return (
-        <div className="flex gap-x-4">
-            <PlacePreviewCover {...place} />
+        <div className="group flex gap-x-4">
+            <Link href={`places/${place.id}`} target="_blank">
+                <PlacePreviewCover {...place} />
+            </Link>
             <div className="flex flex-1 flex-col justify-between">
-                <div className="line-clamp-4 break-words font-medium">{place.title}</div>
+                <Link
+                    href={`places/${place.id}`}
+                    className="link-black line-clamp-4 break-words font-medium group-hover:text-blue-active"
+                    target="_blank"
+                >
+                    {place.title}
+                </Link>
                 <div className="flex items-center justify-between">
                     <PlacePreviewRating {...place} />
                     <PlacePreviewActions {...place} />
