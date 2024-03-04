83c449c1fbc0e1f4303c8cf61eeece70a3504873
Maxim Oskolsky <oskolsky.maxim@gmail.com>
2024-03-04 13:00:58 +0200

    Fix add / delete favorites in visited list

--- src/redux/services/favorites-api.ts
+++ src/redux/services/favorites-api.ts
@@ -20,6 +20,7 @@ export const favoritesAPI = api.injectEndpoints({
                 { type: 'Favorites' },
                 { type: 'Places' },
                 { type: 'PlacesMeta', id: placeId },
+                { type: 'Visited' },
             ],
         }),
 
@@ -32,6 +33,7 @@ export const favoritesAPI = api.injectEndpoints({
                 { type: 'Favorites' },
                 { type: 'Places' },
                 { type: 'PlacesMeta', id: placeId },
+                { type: 'Visited' },
             ],
         }),
     }),
