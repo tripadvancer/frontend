6cda95cff86d02669704d9bc3fe7e794e30906e2
Maxim Oskolsky <oskolsky.maxim@gmail.com>
2024-03-23 12:07:54 +0200

    Hot fix

--- src/components/features/email-verification/email-verification.tsx
+++ src/components/features/email-verification/email-verification.tsx
@@ -8,6 +8,8 @@ import { EmailVerificationNotice } from './email-verification-notice'
 export const EmailVerification = async () => {
     const { session, hasToken } = await getSSRSession()
 
+    console.log(session)
+
     if (!session && hasToken) {
         return <TryRefreshComponent />
     }
