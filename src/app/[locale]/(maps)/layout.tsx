0d4e33aad8554c8e3e84deaa39c2bc66c7460ca6
Maxim Oskolsky <oskolsky.maxim@gmail.com>
2024-03-27 12:28:18 +0200

    Minor fixes

--- src/app/[locale]/(maps)/layout.tsx
+++ src/app/[locale]/(maps)/layout.tsx
@@ -12,11 +12,21 @@ export const metadata: Metadata = {
         description:
             'Convenient search for places on the map, managing favorite spots, as well as finding a random location for an unforgettable journey.',
         url: '/maps',
+        images: [
+            {
+                url: '/images/og-image-2.jpg',
+                width: 1200,
+                height: 628,
+                type: 'image/jpeg',
+                alt: 'Tripadvancer',
+            },
+        ],
     },
     twitter: {
         title: 'Maps',
         description:
             'Convenient search for places on the map, managing favorite spots, as well as finding a random location for an unforgettable journey.',
+        images: '/images/og-image-2.jpg',
     },
 }
 
