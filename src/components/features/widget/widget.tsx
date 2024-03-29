f76490bc3c7d29b3ee3e2deda3ef51c5228fcfd1
Maxim Oskolsky <oskolsky.maxim@gmail.com>
2024-03-29 22:44:54 +0200

    Fix widget scroll

--- src/components/features/widget/widget.tsx
+++ src/components/features/widget/widget.tsx
@@ -5,8 +5,8 @@ import { WidgetHeader } from './components/widget-header'
 export const Widget = () => {
     return (
         <>
-            <div className="pointer-events-none fixed right-0 top-0 z-40 h-full w-full overflow-y-auto pb-8 scrollbar-hide sm:w-[512px] sm:p-8">
-                <div className="pointer-events-auto rounded-b-2xl bg-white shadow-large sm:rounded-2xl">
+            <div className="fixed right-0 top-0 z-40 max-h-full w-full overflow-y-auto pb-8 scrollbar-hide sm:w-[512px] sm:p-8">
+                <div className="rounded-b-2xl bg-white shadow-large sm:rounded-2xl">
                     <WidgetHeader />
                     <WidgetBody />
                 </div>
