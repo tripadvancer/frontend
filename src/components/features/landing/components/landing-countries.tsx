aa4863f5972d23bd022b5630063fa88802838fd0
Maxim Oskolsky <oskolsky.maxim@gmail.com>
2024-04-06 20:54:59 +0300

    Revert

--- src/components/features/landing/components/landing-countries.tsx
+++ src/components/features/landing/components/landing-countries.tsx
@@ -1,5 +1,4 @@
 import { getCountries } from '@/services/countries'
-import { getUserCountryCode } from '@/services/edge-geo'
 import { getI18n } from '@/utils/i18n/i18n.server'
 
 import { LandingCountriesFeed } from './landing-countries-feed'
@@ -7,18 +6,6 @@ import { LandingCountriesFeed } from './landing-countries-feed'
 export const LandingCountries = async () => {
     const t = await getI18n()
     const countries = await getCountries()
-    const userCountryCode = await getUserCountryCode()
-
-    // Determine user's country
-    const userCountry = countries.find(country => country.countryCode.toLowerCase() === userCountryCode)
-
-    console.log(countries)
-    console.log(userCountryCode, userCountry)
-
-    // Sort countries accordingly
-    const sortCountries = userCountry
-        ? [userCountry, ...countries.filter(country => country.countryCode.toLowerCase() !== userCountryCode)]
-        : countries
 
     return (
         <section>
@@ -26,7 +13,7 @@ export const LandingCountries = async () => {
             <p className="m-auto mb-16 w-full text-center text-big text-black-70 sm:w-2/3">
                 {t('pages.landing.countries.description')}
             </p>
-            <LandingCountriesFeed countries={sortCountries} />
+            <LandingCountriesFeed countries={countries} />
         </section>
     )
 }
