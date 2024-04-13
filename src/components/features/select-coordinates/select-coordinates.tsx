480390432fe187331b2842050b516ee39f02b8f4
Maxim Oskolsky <oskolsky.maxim@gmail.com>
2024-04-13 13:52:27 +0300

    Enter location dialog

--- src/components/features/select-coordinates/select-coordinates.tsx
+++ src/components/features/select-coordinates/select-coordinates.tsx
@@ -2,6 +2,8 @@
 
 import { ChangeEvent } from 'react'
 
+import Image from 'next/image'
+
 import { FormButton } from '@/components/ui/form-button'
 import { FormInput } from '@/components/ui/form-input'
 import { useDialog } from '@/providers/dialog-provider'
@@ -13,17 +15,25 @@ export const SelectCoordinates = () => {
 
     return (
         <div className="flex w-full flex-col gap-y-8 sm:w-104">
-            <h1 className="h7 text-center">Location</h1>
+            <h1 className="h7 text-center">Select place location</h1>
             <FormInput
                 type={'text'}
                 name={''}
                 value={''}
-                placeholder={''}
+                placeholder="Enter address or coordinates"
                 onChange={function (e: ChangeEvent<HTMLInputElement>): void {
                     throw new Error('Function not implemented.')
                 }}
             />
-            <div className="h-96 w-full bg-black-15"></div>
+            <div className="relative h-96 w-full bg-black-15">
+                <Image
+                    src="images/pin-blue.svg"
+                    width={27}
+                    height={41}
+                    className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 transform"
+                    alt=""
+                />
+            </div>
             <FormButton type="stroke" onClick={() => dialog.close()}>
                 {t('common.action.confirm')}
             </FormButton>
--- src/locales/en.ts
+++ src/locales/en.ts
@@ -115,7 +115,7 @@ export default {
     'placeholder.action.email': 'Enter email address',
     'placeholder.action.new_email': 'Enter new email address',
     'placeholder.place.title': 'Enter place title',
-    'placeholder.place.coordinates': 'Enter location',
+    'placeholder.place.coordinates': 'Select place location',
 
     'success.send_complaint': 'Your complaint has been sent.',
     'success.copy_coordinates': 'Coordinates have been copied to the clipboard.',
