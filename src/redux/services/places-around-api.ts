ce399563b9b6c2f0a286cf511fbaa6bb31c194e7
Maxim Oskolsky <oskolsky.maxim@gmail.com>
2024-04-22 20:32:53 +0300

    Fixes

--- src/redux/services/places-around-api.ts
+++ src/redux/services/places-around-api.ts
@@ -2,24 +2,16 @@ import type { IPlaceNearby, IRandomPlace } from '@/utils/types/place'
 
 import { api } from './api'
 
-interface GetRandomPlaceParams {
-    lat: number
-    lng: number
-    radius: number
-    categories: number[]
-}
-
 interface GetPlacesAroundParams {
     lat: number
     lng: number
     radius: number
     categories: number[]
-    limit?: number
 }
 
 export const placesAroundAPI = api.injectEndpoints({
     endpoints: build => ({
-        getRandomPlace: build.query<IRandomPlace, GetRandomPlaceParams>({
+        getRandomPlace: build.query<IRandomPlace, GetPlacesAroundParams>({
             query: params => ({
                 url: 'places-around/random',
                 params: {
@@ -38,7 +30,6 @@ export const placesAroundAPI = api.injectEndpoints({
                     lat: params.lat,
                     radius: params.radius,
                     categories_ids: params.categories.join(),
-                    limit: params.limit || '',
                 },
             }),
         }),
