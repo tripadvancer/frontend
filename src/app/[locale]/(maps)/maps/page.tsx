a5988f0625d47902eb9c37c01a955bb023c40e70
Maxim Oskolsky <oskolsky.maxim@gmail.com>
2024-04-24 10:25:08 +0300

    Maps layout improvements

--- src/app/[locale]/(maps)/maps/page.tsx
+++ src/app/[locale]/(maps)/maps/page.tsx
@@ -1,11 +1,43 @@
+import type { Metadata } from 'next/types'
+
 import { CookieConsent } from '@/components/features/cookie-consent/cookie-consent'
 import { Mapbox } from '@/components/features/map/mapbox'
 import { Onboarding } from '@/components/features/onboarding/onboarding'
 import { WidgetCommon } from '@/components/features/widget/widget-common'
 
+export const metadata: Metadata = {
+    title: 'Maps',
+    description:
+        'Convenient search for places on the map, managing favorite spots, as well as finding a random location for an unforgettable journey.',
+    alternates: {
+        canonical: '/maps',
+    },
+    openGraph: {
+        title: 'Maps',
+        description:
+            'Convenient search for places on the map, managing favorite spots, as well as finding a random location for an unforgettable journey.',
+        url: '/maps',
+        images: [
+            {
+                url: '/images/og-image-2.jpg',
+                width: 1200,
+                height: 628,
+                type: 'image/jpeg',
+                alt: 'Tripadvancer',
+            },
+        ],
+    },
+    twitter: {
+        title: 'Maps',
+        description:
+            'Convenient search for places on the map, managing favorite spots, as well as finding a random location for an unforgettable journey.',
+        images: '/images/og-image-2.jpg',
+    },
+}
+
 export default function MapsPage() {
     return (
-        <div className="h-dvh w-dvw border">
+        <div className="h-dvh w-dvw">
             <WidgetCommon />
             <Mapbox />
             <Onboarding />
