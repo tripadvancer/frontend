0ab23387fe7dc00dfe8a1786ba2c553a4437becb
Maxim Oskolsky <oskolsky.maxim@gmail.com>
2024-04-28 16:13:26 +0300

    Change footer

--- src/components/features/footer/footer.tsx
+++ src/components/features/footer/footer.tsx
@@ -1,5 +1,6 @@
 import Link from 'next/link'
 
+import { TwitterIcon24 } from '@/components/ui/icons'
 import { getI18n } from '@/utils/i18n/i18n.server'
 
 export const Footer = async () => {
@@ -9,18 +10,25 @@ export const Footer = async () => {
         <footer className="bg-white">
             <div className="container">
                 <div className="border-t border-black-70" />
-                <div className="inner-container py-8">
-                    <section className="mb-5 text-center text-black-70">
-                        <h5 className="font-medium">{t('about.title')}</h5>
-                        <p>{t('about.description')}</p>
-                    </section>
+                <div className="inner-container flex flex-col items-center gap-y-4 py-8">
                     <nav className="flex flex-row flex-wrap justify-center gap-x-2 text-black-40">
                         <Link href={`mailto:${process.env.NEXT_PUBLIC_EMAIL}`}>Contact Us</Link>•
                         <Link href="/legal/terms-and-conditions">Terms and Conditions</Link>•
                         <Link href="/legal/privacy-policy">Privacy Policy</Link>•
                         <Link href="/legal/cookie-policy">Cookie Policy</Link>
                     </nav>
-                    <div className="text-center text-black-40">© Tripadvancer, 2024</div>
+
+                    <Link href="https://twitter.com/tripadvancer_me" className="link-black" target="_blank">
+                        <TwitterIcon24 />
+                    </Link>
+
+                    <div className="text-center">
+                        Powered by{' '}
+                        <Link href="https://stadiamaps.com/" target="_blank">
+                            Stadiamaps
+                        </Link>
+                        <div className="text-black-40">© Tripadvancer, 2024</div>
+                    </div>
                 </div>
             </div>
         </footer>
