d79d75d417847a7cda4c53f6ec53c0b8f711e152
Maxim Oskolsky <oskolsky.maxim@gmail.com>
2024-05-01 20:31:56 +0300

    Fix share button

--- src/components/features/place/components/place-sidebar-actions-share.tsx
+++ src/components/features/place/components/place-sidebar-actions-share.tsx
@@ -1,17 +1,37 @@
 'use client'
 
+import { isMobile } from 'react-device-detect'
+
 import type { IPlace } from '@/utils/types/place'
 
 import { SharePlace } from '@/components/features/share-place/share-place'
 import { ShareIcon24 } from '@/components/ui/icons'
 import { useDialog } from '@/providers/dialog-provider'
+import { useToast } from '@/providers/toast-provider'
 import { useI18n } from '@/utils/i18n/i18n.client'
 
 export const PlaceSidebarActionsShare = ({ place }: { place: IPlace }) => {
     const t = useI18n()
     const dialog = useDialog()
+    const toast = useToast()
 
-    const handleClick = () => {
+    const handleClick = async () => {
+        if (isMobile) {
+            const shareData = {
+                title: place.title,
+                text: 'Look what place I found on Tripadvancer!',
+                url: `${process.env.NEXT_PUBLIC_WEBSITE_DOMAIN}/places/${place.id}`,
+            }
+
+            try {
+                await navigator.share(shareData)
+            } catch (err) {
+                toast.error(t('common.error'))
+            }
+
+            return
+        }
+
         dialog.open(<SharePlace {...place} />)
     }
 
