305e8866e0273d58a08fc83a4b3a52f16c550e91
Maxim Oskolsky <oskolsky.maxim@gmail.com>
2024-05-01 19:37:43 +0300

    New yandex link

--- src/utils/helpers/navigation.ts
+++ src/utils/helpers/navigation.ts
@@ -8,5 +8,5 @@ export const navigationProviders: NavigationProvider = {
     google: (lngLat: LngLat) => `https://google.com/maps/dir//${lngLat.lat},${lngLat.lng}`,
     waze: (lngLat: LngLat) => `https://waze.com/ul?ll=${lngLat.lat},${lngLat.lng}&navigate=yes`,
     apple: (lngLat: LngLat) => `https://maps.apple.com/?ll=${lngLat.lat},${lngLat.lng}`,
-    yandex: (lngLat: LngLat) => `https://yandex.com/maps/?ll=${lngLat.lng},${lngLat.lat}&z=14&l=map`,
+    yandex: (lngLat: LngLat) => `https://maps.yandex.ru/?text=${lngLat.lat}+${lngLat.lng}`,
 }
