8589f07a0aa425bb0de86952b4c439e63f31c99f
Maxim Oskolsky <oskolsky.maxim@gmail.com>
2024-05-02 17:50:48 +0300

    Fix

--- src/components/features/place/components/place-main-reviews.tsx
+++ src/components/features/place/components/place-main-reviews.tsx
@@ -19,12 +19,12 @@ export const PlaceMainReviews = async ({ place, activeUserId, isAuth, isEmailVer
         <section className="flex scroll-m-24 flex-col gap-y-8 sm:scroll-m-28" id="reviews">
             <h2 className="h5">{t('pages.place.reviews.title')}</h2>
             <div className="flex flex-col gap-y-8">
-                {/* <PlaceMainOwnReview
+                <PlaceMainOwnReview
                     place={place}
                     activeUserId={activeUserId}
                     isAuth={isAuth}
                     isEmailVerified={isEmailVerified}
-                /> */}
+                />
                 <PlaceMainReviewsFeed placeId={place.id} activeUserId={activeUserId} isAuth={isAuth} />
             </div>
         </section>
