4988f48e5143fa81fa47eead9acebbc56798005c
Maxim Oskolsky <oskolsky.maxim@gmail.com>
2024-05-02 09:20:10 +0300

    Lang changer improvement

--- src/components/features/language-changer/language-changer.tsx
+++ src/components/features/language-changer/language-changer.tsx
@@ -1,5 +1,7 @@
 'use client'
 
+import { useMediaQuery } from 'usehooks-ts'
+
 import { Dropdown } from '@/components/ui/dropdown'
 import { GlobeIcon16 } from '@/components/ui/icons'
 import { useChangeLocale, useCurrentLocale } from '@/utils/i18n/i18n.client'
@@ -7,6 +9,7 @@ import { useChangeLocale, useCurrentLocale } from '@/utils/i18n/i18n.client'
 export const LanguageChanger = () => {
     const changeLocale = useChangeLocale()
     const currentLocale = useCurrentLocale()
+    const isMobile = useMediaQuery('(max-width: 639px)')
 
     const handleChangeLocale = (locale: 'en' | 'ru') => {
         if (currentLocale !== locale) {
@@ -28,12 +31,15 @@ export const LanguageChanger = () => {
                     onClick: () => handleChangeLocale('ru'),
                 },
             ]}
+            position={isMobile ? 'center' : 'right'}
             currentItem={currentLocale}
         >
             <div className="link flex-center gap-x-2">
                 {currentLocale === 'en' && 'English'}
                 {currentLocale === 'ru' && 'Русский'}
-                <GlobeIcon16 />
+                <div className="hidden sm:block">
+                    <GlobeIcon16 />
+                </div>
             </div>
         </Dropdown>
     )
