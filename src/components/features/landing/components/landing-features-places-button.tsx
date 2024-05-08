ee5fa045f63a79356e0e2d1bd28ad96f21e9e648
Maxim Oskolsky <oskolsky.maxim@gmail.com>
2024-05-09 01:41:16 +0300

    Fix

--- src/components/features/landing/components/landing-features-places-button.tsx
+++ src/components/features/landing/components/landing-features-places-button.tsx
@@ -3,18 +3,13 @@
 import { useRouter } from 'next/navigation'
 
 import { FormButton } from '@/components/ui/form-button'
-import { openWidget, resetWidgetState } from '@/redux/features/widget-slice'
-import { useAppDispatch } from '@/redux/hooks'
 import { useI18n } from '@/utils/i18n/i18n.client'
 
 export const LandingFeaturesPlacesButton = () => {
     const t = useI18n()
     const router = useRouter()
-    const dispatch = useAppDispatch()
 
     const handleClick = () => {
-        dispatch(resetWidgetState())
-        dispatch(openWidget())
         router.push('/maps')
     }
 
