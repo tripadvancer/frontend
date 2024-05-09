e8bcab8c8ffcd0a60e77adea994e48d671a377b6
Maxim Oskolsky <oskolsky.maxim@gmail.com>
2024-05-09 09:48:23 +0300

    Fix widget

--- src/components/features/maps-container/maps-container.tsx
+++ src/components/features/maps-container/maps-container.tsx
@@ -72,7 +72,7 @@ export const MapsContainer = ({ map, header, widget }: { map: ReactNode; header:
     return (
         <div className="size-full">
             {map}
-            <div className="scrollbar-hide fixed right-0 top-0 z-40 max-h-full w-[496px] overflow-y-auto overscroll-none pb-8 pl-8 pr-4 pt-4">
+            <div className="scrollbar-hide fixed right-0 top-0 z-40 max-h-full w-[496px] overflow-y-auto overscroll-y-none pb-8 pl-8 pr-4 pt-4">
                 <div className="overflow-hidden rounded-2xl shadow-large">{widget}</div>
             </div>
         </div>
--- src/components/features/widget/components/widget-places.tsx
+++ src/components/features/widget/components/widget-places.tsx
@@ -1,42 +1,21 @@
 'use client'
 
-import { useMediaQuery } from 'usehooks-ts'
-
-import { ChevronBottomIcon16, ChevronTopIcon16 } from '@/components/ui/icons'
-import { getWidgetState, toggleWidgetPlacesOpened } from '@/redux/features/widget-slice'
-import { useAppDispatch, useAppSelector } from '@/redux/hooks'
+import { getWidgetState } from '@/redux/features/widget-slice'
+import { useAppSelector } from '@/redux/hooks'
 import { WidgetTabsEnum } from '@/utils/enums'
-import { useI18n } from '@/utils/i18n/i18n.client'
 
 import { WidgetAllPlaces } from './widget-all-places'
 import { WidgetSaved } from './widget-saved/widget-saved'
 import { WidgetTabs } from './widget-tabs'
 
 export const WidgetPlaces = ({ isAuth }: { isAuth: boolean }) => {
-    const t = useI18n()
-    const dispatch = useAppDispatch()
     const widgetState = useAppSelector(getWidgetState)
-    const isMobile = useMediaQuery('(max-width: 639px)')
 
     return (
-        <div className="flex flex-col gap-y-4">
-            <div
-                className="flex items-center justify-between sm:cursor-pointer"
-                onClick={isMobile ? undefined : () => dispatch(toggleWidgetPlacesOpened())}
-            >
-                <div className="text-caps uppercase">{t('widget.places.title')}</div>
-                <div className="hidden items-center justify-center gap-2 sm:flex">
-                    {widgetState.isPlacesOpened ? <ChevronTopIcon16 /> : <ChevronBottomIcon16 />}
-                </div>
-            </div>
-
-            {widgetState.isPlacesOpened && (
-                <div className="flex flex-1 flex-col gap-y-6 sm:gap-y-8">
-                    <WidgetTabs />
-                    {widgetState.activeTab === WidgetTabsEnum.ALL && <WidgetAllPlaces />}
-                    {widgetState.activeTab === WidgetTabsEnum.SAVED && <WidgetSaved isAuth={isAuth} />}
-                </div>
-            )}
+        <div className="flex flex-1 flex-col gap-y-6 sm:gap-y-8">
+            <WidgetTabs />
+            {widgetState.activeTab === WidgetTabsEnum.ALL && <WidgetAllPlaces />}
+            {widgetState.activeTab === WidgetTabsEnum.SAVED && <WidgetSaved isAuth={isAuth} />}
         </div>
     )
 }
