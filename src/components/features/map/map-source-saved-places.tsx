4c41c543a34d591dfeeff436735a9251052399e9
Maxim Oskolsky <oskolsky.maxim@gmail.com>
2024-05-12 12:50:15 +0300

    .

--- src/components/features/map/map-source-saved-places.tsx
+++ src/components/features/map/map-source-saved-places.tsx
@@ -1,23 +1,42 @@
 'use client'
 
-import { Layer, Source } from 'react-map-gl/maplibre'
+import { useEffect, useMemo } from 'react'
+import { Layer, Source, useMap } from 'react-map-gl/maplibre'
+
+import { useMediaQuery } from 'usehooks-ts'
 
 import { getWidgetSelectedCategories } from '@/redux/features/widget-slice'
 import { useAppSelector } from '@/redux/hooks'
 import { listAPI } from '@/redux/services/list-api'
+import { arrayToLngLat, getBoundsFromCoordinates, getMapFlyToOptions } from '@/utils/helpers/maps'
 
 import { placesLayer } from './map-layers'
 
 export const MapSourceSavedPlaces = ({ listId }: { listId: number }) => {
+    const isMobile = useMediaQuery('(max-width: 639px)')
     const selectedCategories = useAppSelector(getWidgetSelectedCategories)
-    const { data: places, isSuccess } = listAPI.useGetListPlacesQuery({ listId, selectedCategories })
+
+    const { data, isSuccess } = listAPI.useGetListPlacesQuery({ listId, selectedCategories })
+
+    const places = useMemo(() => data?.features.map(({ properties }) => properties) ?? [], [data])
+
+    const { map } = useMap()
+
+    useEffect(() => {
+        if (!isMobile && isSuccess && places.length > 0) {
+            if (places.length === 1) {
+                const lngLat = arrayToLngLat(places[0].coordinates)
+                map?.flyTo(getMapFlyToOptions(lngLat))
+                return
+            }
+
+            const bounds = getBoundsFromCoordinates(places.map(place => place.coordinates))
+            map?.fitBounds(bounds)
+        }
+    }, [isMobile, isSuccess, places, map])
 
     return (
-        <Source
-            id="places-source"
-            type="geojson"
-            data={isSuccess ? places : { type: 'FeatureCollection', features: [] }}
-        >
+        <Source id="places-source" type="geojson" data={isSuccess ? data : { type: 'FeatureCollection', features: [] }}>
             <Layer {...placesLayer} />
         </Source>
     )
--- src/components/features/widget/components/widget-saved/widget-saved-lists-view-places-feed.tsx
+++ src/components/features/widget/components/widget-saved/widget-saved-lists-view-places-feed.tsx
@@ -1,15 +1,10 @@
 'use client'
 
-import { useEffect, useMemo } from 'react'
-import { useMap } from 'react-map-gl/maplibre'
+import { useMemo } from 'react'
 
-import { useMediaQuery } from 'usehooks-ts'
-
-import { getIsFilterMapBySavedLists } from '@/redux/features/map-slice'
 import { getWidgetSelectedCategories } from '@/redux/features/widget-slice'
 import { useAppSelector } from '@/redux/hooks'
 import { listAPI } from '@/redux/services/list-api'
-import { arrayToLngLat, getBoundsFromCoordinates, getMapFlyToOptions } from '@/utils/helpers/maps'
 import { useI18n } from '@/utils/i18n/i18n.client'
 
 import { WidgetMessage } from '../widget-message'
@@ -19,29 +14,13 @@ import { WidgetPlacesFeedSkeleton } from '../widget-places-feed/widget-places-fe
 export const WidgetSavedListsViewPlacesFeed = ({ listId }: { listId: number }) => {
     const t = useI18n()
     const selectedCategories = useAppSelector(getWidgetSelectedCategories)
-    const isMobile = useMediaQuery('(max-width: 639px)')
-    const isMapFilterByList = useAppSelector(getIsFilterMapBySavedLists)
 
-    const { map } = useMap()
     const { data, isError, isLoading, isSuccess, refetch } = listAPI.useGetListPlacesQuery({
         listId,
         selectedCategories,
     })
 
     const places = useMemo(() => data?.features.map(({ properties }) => properties) ?? [], [data])
-
-    useEffect(() => {
-        if (!isMobile && isMapFilterByList && isSuccess && places.length > 0) {
-            if (places.length === 1) {
-                const lngLat = arrayToLngLat(places[0].coordinates)
-                map?.flyTo(getMapFlyToOptions(lngLat))
-                return
-            }
-
-            const bounds = getBoundsFromCoordinates(places.map(place => place.coordinates))
-            map?.fitBounds(bounds)
-        }
-    }, [isMobile, isMapFilterByList, isSuccess, places, map])
 
     if (isError) {
         return <WidgetMessage onAction={refetch} isLoading={isLoading} />
