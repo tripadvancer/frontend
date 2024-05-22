0fea9566cd06ea74762ff81c2afabfd9680b2c59
Maxim Oskolsky <oskolsky.maxim@gmail.com>
2024-05-22 14:03:40 +0300

    Merge develop

--- src/components/features/map/sources/map-source-random.tsx
+++ src/components/features/map/sources/map-source-random.tsx
@@ -6,8 +6,6 @@ import { Layer, Source, useMap } from 'react-map-gl/maplibre'
 import { circle } from '@turf/turf'
 import { useMediaQuery } from 'usehooks-ts'
 
-import type { LngLat } from '@/utils/types/geo'
-
 import { getUserLocation } from '@/redux/features/user-slice'
 import { getWidgetRandomRadius } from '@/redux/features/widget-slice'
 import { useAppSelector } from '@/redux/hooks'
@@ -15,31 +13,30 @@ import { LngLatToArray, getBoundsFromCoordinates } from '@/utils/helpers/maps'
 
 import { circleLayer, placesLayer } from './map-layers'
 
-export const MapSourceRandomContainer = () => {
+export const MapSourceRandom = () => {
     const userLocation = useAppSelector(getUserLocation)
-
-    if (!userLocation) {
-        return null
-    }
-
-    return <MapSourceRandom userLocation={userLocation} />
-}
-
-const MapSourceRandom = ({ userLocation }: { userLocation: LngLat }) => {
     const radius = useAppSelector(getWidgetRandomRadius)
     const isMobile = useMediaQuery('(max-width: 639px)')
 
     const { map } = useMap()
 
+    useEffect(() => {
+        if (userLocation && map) {
+            const bounds = getBoundsFromCoordinates(geoJson.geometry.coordinates[0])
+            map.fitBounds(bounds, {
+                animate: false,
+            })
+        }
+    }, [userLocation, isMobile, map, radius])
+
+    if (!userLocation) {
+        return null
+    }
+
     const geoJson = circle(LngLatToArray(userLocation), radius, {
         steps: 50,
         units: 'kilometers',
     })
-
-    useEffect(() => {
-        const bounds = getBoundsFromCoordinates(geoJson.geometry.coordinates[0])
-        map?.fitBounds(bounds, { animate: false })
-    }, [isMobile, map, radius, geoJson])
 
     return (
         <>
