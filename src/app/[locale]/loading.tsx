cefcd451cc9788064e1defd690b19ff2e53be68a
Maxim Oskolsky <oskolsky.maxim@gmail.com>
2024-06-05 17:37:58 +0300

    Fix global map pins

--- src/app/[locale]/loading.tsx
+++ src/app/[locale]/loading.tsx
@@ -2,7 +2,7 @@ import { Spinner } from '@/components/ui/spinner'
 
 export default function Loading() {
     return (
-        <div className="flex-center h-screen w-screen bg-blue-20 text-white">
+        <div className="flex-center relative z-50 h-screen w-screen bg-blue-20 text-white">
             <Spinner size={96} />
         </div>
     )
