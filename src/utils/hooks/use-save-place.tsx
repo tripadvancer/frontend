134230d587a767e3a041c3dc31c42a612355e11d
Maxim Oskolsky <oskolsky.maxim@gmail.com>
2024-08-23 12:05:50 +0300

    Change widget place actions

--- src/utils/hooks/use-save-place.tsx
+++ src/utils/hooks/use-save-place.tsx
@@ -7,13 +7,13 @@ import { SavePlace } from '@/components/features/dialogs/save-place/save-space'
 import { useDialog } from '@/providers/dialog-provider'
 
 interface useSavePlaceInterface {
-    toggle: () => void
+    savePlace: () => void
 }
 
 export function useSavePlace(placeId: number): useSavePlaceInterface {
     const dialog = useDialog()
 
-    const toggle = async (): Promise<void> => {
+    const savePlace = async (): Promise<void> => {
         const doesSessionExist = await Session.doesSessionExist()
 
         if (!doesSessionExist) {
@@ -24,5 +24,5 @@ export function useSavePlace(placeId: number): useSavePlaceInterface {
         dialog.open(<SavePlace placeId={placeId} />)
     }
 
-    return { toggle }
+    return { savePlace }
 }
