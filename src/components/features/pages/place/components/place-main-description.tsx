fbe29a3d2fce26b33a071906ac660b5cfb21fe4b
Maxim Oskolsky <oskolsky.maxim@gmail.com>
2025-05-06 09:05:20 +0300

    Disable translate service

--- src/components/features/pages/place/components/place-main-description.tsx
+++ src/components/features/pages/place/components/place-main-description.tsx
@@ -34,14 +34,14 @@ export const PlaceMainDescription = ({ description }: PlaceMainDescriptionProps)
             <h2 className="h5">About</h2>
 
             <div className="flex flex-col gap-y-4">
-                <Translate
+                {/* <Translate
                     originalText={description}
                     availableTargets={[
                         { label: 'Русский', code: 'ru' },
                         { label: 'English', code: 'en' },
                     ]}
                     onTranslate={setDisplayText}
-                />
+                /> */}
 
                 <div className="break-words text-big" dangerouslySetInnerHTML={{ __html: sanitizedHTML }} />
             </div>
