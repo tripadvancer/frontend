04677d022d6e5177d97b3ccfdcffd23cdb7e20e6
Maxim Oskolsky <oskolsky.maxim@gmail.com>
2025-06-05 09:27:35 +0300

    Fixes

--- src/components/features/common/review/components/review-rating-place.tsx
+++ src/components/features/common/review/components/review-rating-place.tsx
@@ -1,18 +1,10 @@
-'use client'
-
-import { useLocale } from 'next-intl'
-
 import Link from 'next/link'
 
+import { PlacePreviewMeta } from '@/components/ui/place-preview-meta'
 import { Rating } from '@/components/ui/rating'
-import { getCountryByCode } from '@/services/countries'
-import { formattedDate } from '@/utils/helpers/common'
 import { IReview } from '@/utils/types/common'
 
 export const ReviewRatingPlace = ({ place, rating, createdAt }: IReview) => {
-    const locale = useLocale()
-    const country = getCountryByCode(place.countryCode)
-
     return (
         <div className="flex flex-col gap-y-1 overflow-hidden">
             <Rating value={rating} size={16} />
@@ -20,17 +12,7 @@ export const ReviewRatingPlace = ({ place, rating, createdAt }: IReview) => {
                 <Link href={`/places/${place.id}`} className="break-words text-small-bold text-black-70">
                     {place.title}
                 </Link>
-                <div className="flex gap-x-2">
-                    {country && (
-                        <>
-                            <Link href={`/countries/${country.slug}`} className="text-small text-black-40">
-                                {country.name[locale]}
-                            </Link>
-                            <div className="text-small text-black-40">•</div>
-                        </>
-                    )}
-                    <div className="text-small text-black-40">{formattedDate(createdAt, locale)}</div>
-                </div>
+                <PlacePreviewMeta countryCode={place.countryCode} createdAt={createdAt} />
             </div>
         </div>
     )
