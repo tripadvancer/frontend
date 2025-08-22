2f8b9674eb0e5fff94a913f2f3ad782bb9482dcf
Maxim Oskolsky <oskolsky.maxim@gmail.com>
2025-08-22 14:34:44 +0300

    Fixes

--- src/components/features/layout/header/components/header-logo.tsx
+++ src/components/features/layout/header/components/header-logo.tsx
@@ -5,7 +5,13 @@ export const HeaderLogo = () => {
     return (
         <Link href="/" className="flex flex-none">
             {/* <Image src="/images/logo-mini.svg" width="32" height="28" alt="Tripadvancer" className="hidden md:block" /> */}
-            <Image src="/images/logo.svg" width="187" height="32" alt="Tripadvancer" />
+            <Image
+                src="/images/logo.svg"
+                width="187"
+                height="32"
+                alt="Tripadvancer"
+                className="h-[24px] w-[140px] sm:h-[32px] sm:w-[187px]"
+            />
         </Link>
     )
 }
